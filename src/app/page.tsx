a903a8736964aa761480f74c33e38a52c3107b09
MariCapelo <marina.gac30@gmail.com>
2024-08-20 22:23:52 -0300

    Ready Components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,31 +5,7 @@ import Counter from "./components/Counter/Counter";
 
 export default function Home() {
   return (
-    <main className="something">
-      <NavBar></NavBar>
-      <div className='ExtraButtons'>
-        <p className='title'>Extra Buttons</p>
-        <Button.Green extra>Button</Button.Green>
-        <Button.DarkGreen extra>Button</Button.DarkGreen>
-        <br/>
-        <Button.Disable extra>Disable</Button.Disable>
-        <Button.Focused extra>Focused</Button.Focused>
-        <br/>
-        <Button.Attention extra>Warning</Button.Attention>
-      </div>
-
-      <div className='NormalButtons'>
-        <p className='title'>Small Buttons</p>
-        <Button.Green>Button</Button.Green>
-        <Button.DarkGreen>Button</Button.DarkGreen>
-        <br/>
-        <Button.Disable>Disable</Button.Disable>
-        <Button.Focused>Focused</Button.Focused>
-        <br/>
-        <Button.Attention>Warning</Button.Attention>
-      </div>
-      <Counter/>
-    </main>
+    <></>
   );
 }
 // 
