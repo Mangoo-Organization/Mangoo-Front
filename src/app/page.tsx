7fc6babc4d3ede74d81a4a8a862aae3f944a79d5
Clara Figueiredo <clara.figueiredo.contas@gmail.com>
2024-09-03 15:31:30 -0300

    creation screen student register

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,17 +9,11 @@ import SecondModal from './components/Modals/SecondModal';
 import ThirdModal from './components/Modals/ThirdModal';
 import FourthModal from './components/Modals/FourthModal';
 import ProfilePhoto from "./components/ProfilePhoto/ProfilePhoto";
+import Information from "./information/page";
+import StudentRegister from "./student-register/page";
 
 export default function Home() {
   return (
-    <div>
-      {/* Atualize para retornar os componentes necessários */}
-      <NavBar />
-      <Counter />
-      <InputSimple />
-      <Checkbox />
-      <ProfilePhoto />
-      {/* Atualize para retornar os componentes necessários */}
-    </div>
+    <StudentRegister></StudentRegister>
   );
 }
---
+++ src/app/student-register/page.tsx
@@ -0,0 +1,69 @@
+'use client'
+import React from 'react'
+import './style.css'
+import NavBar from '../components/NavBar/NavBar'
+import Image from 'next/image'
+import { InputSimple } from '../components/InputSimple/InputSimple'
+import * as Button from '../components/Button/Button'
+import Dropdown from "../components/Dropdown/Dropdown";
+
+ const StudentRegister = () => {
+  return (
+    <div>
+        <NavBar/>
+        
+    <div className='page__student'>
+
+        <div className='intro__student'>
+            <p>ESPAÇO PARA O CARROSSEL</p>
+        </div>
+
+        <div className='space'>
+
+        </div>
+
+        <div className="student__card">
+            
+            <Image
+                src='../public/icones/mangoo-icon.svg'
+                alt='mangoo icon'
+                width={25}
+                height={25}
+            />
+
+            <p className='titulo__card'>Crie sua conta</p>
+            <div className="div__line"></div>
+
+            <div className="container__inputs">
+
+                <p className='subtitle__text'>Nome completo<span className='asterisk'>*</span></p>
+                <InputSimple extra placeholder='Seu nome'/>
+
+                <p className='subtitle__text'>E-mail<span className='asterisk'>*</span></p>
+                <InputSimple extra placeholder='Seu Email'/>
+
+                <p className='subtitle__text'>Senha<span className='asterisk'>*</span></p>
+                <InputSimple extra placeholder='Digite sua senha'/>
+
+                <p className='subtitle__text'>Confirmação de senha<span className='asterisk'>*</span></p>
+                <InputSimple extra placeholder='Confirme sua senha'/>
+
+                <p className='subtitle__text'>Código da instituição<span className='asterisk'>*</span></p>
+                <InputSimple extra placeholder='Código fornecido pela instituição'/>
+
+                <p className='subtitle__text'>Como podemos te chamar?<span className='asterisk'>*</span></p>
+                <Dropdown options={["opção 1", "opção 2", "opção 3", "opção 4"]}  placeholder="selecione a opção escolhida" title="Como podemos te chamar?" onOptionClick={console.log}/>
+
+            </div>
+
+            <div className="buttons">
+                <Button.Focused extra>Criar conta</Button.Focused>    
+            </div>
+            <p className='footer__text'>Já possui uma conta? Clique aqui</p>
+        </div>
+    </div>
+    </div>
+  )
+}
+
+export default StudentRegister
