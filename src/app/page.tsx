705a82c05a39ea7b6c9adf95c3d6598759d33a40
Clara Figueiredo <clara.figueiredo.contas@gmail.com>
2024-09-17 15:18:40 -0300

    page update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,8 @@ import NavBar from "./components/NavBar/NavBar";
 import Counter from "./components/Counter/Counter";
 import { InputSimple } from "./components/InputSimple/InputSimple";
 import Checkbox from "./components/CheckBox/Checkbox";
-import SecondModal from './components/Modals/SecondModal';
-import ThirdModal from './components/Modals/ThirdModal';
-import FourthModal from './components/Modals/FourthModal';
 import ProfilePhoto from "./components/ProfilePhoto/ProfilePhoto";
 import Information from "./information/page";
-import FirstModal from "./components/Modals/FirstModal";
 import ModalContainer from "./components/Modals/ModalContainer";
 
 export default function Home() {
