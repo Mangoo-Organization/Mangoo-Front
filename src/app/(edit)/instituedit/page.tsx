8399b17037b36990769195c6a0e53fbdf2615fb3
CaioGuerreiro <guerreiro0802@gmail.com>
2024-10-24 16:11:31 -0300

    permission owner done

--- src/app/(edit)/instituedit/page.tsx
+++ src/app/(edit)/instituedit/page.tsx
@@ -1,15 +1,43 @@
 'use client'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import NavBar from '../../components/NavBar/NavBar'
 import SideBar from '../../components/Sidebar/SideBar'
 import './style.css'
 import { InputSimple } from '../../components/InputSimple/InputSimple'
 import * as Button from '../../components/Button/Button'
+import { useRouter } from 'next/navigation'
 
-const page = () => {
+
+const InstituEdit = () => {
+    // Estado de carregamento
+    const [loading, setLoading] = useState(true);
+    const router = useRouter();
+    //dados puxados da api
+    const token = localStorage.getItem('authToken');
+
+
+    useEffect(() =>{
+        const userType = localStorage.getItem('userType');
+
+        if (!token) {
+            // Caso o token não exista, redirecione para a página de login
+            console.error('token não encontrado.');
+            router.push('/without-permission');
+            return;
+          }
+        if (userType !== 'O') {
+            router.push('/without-permission');
+            return;
+          }
+        setLoading(false); // Define o carregamento como concluído
+    },[router]);
+    if (loading) {
+        return <p>Carregando...</p>;
+      }
+
   return (
    <>
-   <NavBar/>
+   <NavBar isLogIn/>
    <div className="instituedit__container">
         <div className="instituedit__sidebar">
             {/* passar os texts de acordo com qual texto você quer sublinhar */}
@@ -106,4 +134,4 @@ const page = () => {
   )
 }
 
-export default page
+export default InstituEdit
